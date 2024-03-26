63d0d839ae2d5d5fb9c4b33aded28dd4ac484631
Bruno Petrus <xpetrus@fi.muni.cz>
2024-03-26 16:26:48 +0100

    wip: db4 and its inverse.

--- wavelets/LiftingStep.py
+++ wavelets/LiftingStep.py
@@ -21,18 +21,13 @@ class LSStep:
         self.max_order = max_order
         self.boundary_condition = boundary_condition
 
-    def evaluate(self, approx: NDArray[int], diff: NDArray[int]) -> NDArray[int]:
+    def evaluate(self, approx: NDArray[int], diff: NDArray[int], inverse:bool = False) -> NDArray[int]:
         n = approx.shape[0]
-        padding = abs(self.max_order)
+        padding = len(self.coefficients)
+        c = -1 if inverse else 1
 
         # TODO: Duplication
         if self.ls_type == LSType.PREDICT:
-            if self.boundary_condition == LSBoundaryCondition.ZERO_PADDING:
-                padded_approx = np.pad(approx, (padding, padding), 'constant',
-                                       constant_values=0)
-            else:
-                raise NotImplementedError(
-                    f"Unknown boundary condition {self.ls_type}")
             predict = 0
             for i in reversed(range(len(self.coefficients))):
                 order = self.max_order - (len(self.coefficients)-i-1)
@@ -40,17 +35,14 @@ class LSStep:
                 print("-------")
                 print(f"[D] order {order}")
                 print(f"[D] coefficient {self.coefficients[i]}")
-                print(f"[D] padded approx {padded_approx[padding+order:padding+order+n]}")
-                predict += self.coefficients[i] * padded_approx[padding+order:padding+order+n]
-            diff += np.floor(predict + 0.5).astype(int)
-        elif self.ls_type == LSType.UPDATE:
-            if self.boundary_condition == LSBoundaryCondition.ZERO_PADDING:
-                padded_diff = np.pad(diff, (padding, padding), 'constant',
-                                       constant_values=0)
-            else:
-                raise NotImplementedError(
-                    f"Unknown boundary condition {self.ls_type}")
 
+                if order >= 0:
+                    predict += self.coefficients[i] * np.pad(approx[order:], (0, order))
+                else:
+                    predict += self.coefficients[i] * np.pad(approx[:order], (-1*order, 0))
+
+            diff += np.floor(predict + 0.5).astype(int) * c
+        elif self.ls_type == LSType.UPDATE:
             update = 0
             for i in reversed(range(len(self.coefficients))):
                 order = self.max_order - (len(self.coefficients)-i-1)
@@ -58,9 +50,11 @@ class LSStep:
                 print("-------")
                 print(f"[D] order {order}")
                 print(f"[D] coefficient {self.coefficients[i]}")
-                print(f"[D] padded diff {padded_diff[padding+order:padding+order+n]}")
-                update += self.coefficients[i] * padded_diff[padding+order:padding+order+n]
-            approx += np.floor(update + 0.5).astype(int)
+                if order >= 0:
+                    update += self.coefficients[i] * np.pad(diff[order:], (0, order))
+                else:
+                    update += self.coefficients[i] * np.pad(diff[:order],(-1*order, 0))
+            approx += np.floor(update + 0.5).astype(int) * c
         return approx, diff
 
 
--- wavelets/db.py
+++ wavelets/db.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def db2_wavelet() -> Wavelet:
+def db2_wavelet(inverse: bool = False) -> Wavelet:
+    c = -1 if inverse else 1
     return [
-        LSStep(LSType.PREDICT, [-1.7321], 0, LSBoundaryCondition.ZERO_PADDING),
-        LSStep(LSType.UPDATE, [0.433, -0.067], 1, LSBoundaryCondition.ZERO_PADDING),
-        LSStep(LSType.PREDICT, [1], -1, LSBoundaryCondition.ZERO_PADDING),
+        LSStep(LSType.PREDICT, [c*(-1.7321)], 0, LSBoundaryCondition.ZERO_PADDING),
+        LSStep(LSType.UPDATE, [c*0.433, c*(-0.067)], 1, LSBoundaryCondition.ZERO_PADDING),
+        LSStep(LSType.PREDICT, [c*1], -1, LSBoundaryCondition.ZERO_PADDING),
     ]
 
 
@@ -17,9 +18,9 @@ def db4_wavelet() -> Wavelet:
     return [
         LSStep(LSType.PREDICT, [-0.3223], 1, LSBoundaryCondition.ZERO_PADDING),
         LSStep(LSType.UPDATE, [-0.3001, -1.1171], 0, LSBoundaryCondition.ZERO_PADDING),
-        LSStep(LSType.PREDICT, [-0.0188, 0.1176], 2, LSBoundaryCondition.ZERO_PADDING),
-        LSStep(LSType.UPDATE, [2.1318, 0.6364], 0, LSBoundaryCondition.ZERO_PADDING),
-        LSStep(LSType.PREDICT, [-0.4691, 0.14, -0.0248], 0, LSBoundaryCondition.ZERO_PADDING),
+        LSStep(LSType.PREDICT, [0.1176, -0.0188], 2, LSBoundaryCondition.ZERO_PADDING),
+        LSStep(LSType.UPDATE, [0.6364, 2.1318], 0, LSBoundaryCondition.ZERO_PADDING),
+        LSStep(LSType.PREDICT, [-0.0248, 0.1400, -0.4691], 0, LSBoundaryCondition.ZERO_PADDING),
     ]
 
 
@@ -45,29 +46,83 @@ def db2_ls_single(array: NDArray[int]) -> Any:
 
     return f_even, f_odd
 
+"""
+LSStep(LSType.PREDICT, [-0.3223], 1, LSBoundaryCondition.ZERO_PADDING),
+LSStep(LSType.UPDATE, [-0.3001, -1.1171], 0, LSBoundaryCondition.ZERO_PADDING),
+LSStep(LSType.PREDICT, [0.1176, -0.0188], 2, LSBoundaryCondition.ZERO_PADDING),
+LSStep(LSType.UPDATE, [0.6364, 2.1318], 0, LSBoundaryCondition.ZERO_PADDING),
+LSStep(LSType.PREDICT, [-0.0248, 0.1400, -0.4691], 0, LSBoundaryCondition.ZERO_PADDING),
+"""
+
+def db4_ls_single(array: NDArray[int]) -> Any:
+    # TODO: remove later....
+    # Divide
+    f_even = array[::2]
+    f_odd = array[1::2]
+
+    # Run LS: Predict, Update, Predict
+    f_odd = f_odd + np.floor(0.5 - 0.3223*np.pad(f_even[1:], (0, 1)))
+    print(f_even, f_odd)
+    f_even = f_even + np.floor(0.5 - 1.1171*f_odd - 0.3001*np.pad(f_odd[:-1], (1, 0)))
+    print(f_even, f_odd)
+    f_odd = f_odd + np.floor(0.5 - 0.0188*np.pad(f_odd[2:], (0, 2)) +
+                             0.1176 * np.pad(f_odd[1:], (0, 1)))
+    print(f_even, f_odd)
+    f_even = f_even + np.floor(0.5 + 2.1318*f_odd + 0.6364*np.pad(f_odd[:-1], (1, 0)))
+    print(f_even, f_odd)
+    f_odd = f_odd + np.floor(0.5 - 0.4691*f_even + 0.14*np.pad(f_even[:-1], (1, 0)) - 0.4691 *np.pad(f_even[:-2], (2, 0)))
+    return f_even, f_odd
+
 
 if __name__ == "__main__":
-    array = np.array([1, 2, 3, 4, 5, 6])
-    print("Original")
-    print(db2_ls_single(array))
+    array = np.array([5, 2, 3, 4, 5, 6, 7, 8])
+    # print("Original")
+    # print(db2_ls_single(array))
+    #
+    # print("Daubechies 2 wavelet integer transform")
+    # print("======================================")
+    # lsw = db2_wavelet()
+    # approx, diff = array[::2], array[1::2]
+    # for step in lsw:
+    #     approx, diff = step.evaluate(approx, diff)
+    #     print(f"approx = {approx}, diff = {diff}")
+    # print(approx)
+    # print(diff)
+    #
+    # print("Inverse")
+    # lsw = db2_wavelet()
+    # cp_approx, cp_diff = approx, diff
+    # for step in reversed(lsw):
+    #     approx, diff = step.evaluate(approx, diff, inverse=True)
+    #     print(f"approx = {approx}, diff = {diff}")
+    # result = np.zeros(array.shape)
+    # result[::2] = approx
+    # result[1::2] = diff
+    # print(result)
 
-    print("Daubechies 2 wavelet integer transform")
+
+    print("\n\n\n\n")
+
+    print(db4_ls_single(array))
+    print("--- .... ----")
+
+    print("Daubechies 4 wavelet integer transform")
     print("======================================")
-    lsw = db2_wavelet()
+
     approx, diff = array[::2], array[1::2]
-    for step in lsw:
+    db4 = db4_wavelet()
+    for step in db4:
         approx, diff = step.evaluate(approx, diff)
-        print(f"approx = {approx}, diff = {diff}")
+        print(approx, diff)
     print(approx)
     print(diff)
 
-    # print("\n\n\n\n")
-    # print("Daubechies 4 wavelet integer transform")
-    # print("======================================")
-    #
-    # approx, diff = array[::2], array[1::2]
-    # db4 = db4_wavelet()
-    # for step in db4:
-    #     approx, diff = step.evaluate(approx, diff)
-    # print(approx)
-    # print(diff)
+    print("Inverse")
+    print("=====================================")
+    db4 = db4_wavelet()
+    for step in reversed(db4):
+        approx, diff = step.evaluate(approx, diff, inverse=True)
+    result = np.zeros(array.shape)
+    result[::2] = approx
+    result[1::2] = diff
+    print(result)
