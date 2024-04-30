36e6292efafe29f99beb4b5267f53de032ba509f
Bruno Petrus <xpetrus@fi.muni.cz>
2024-04-30 15:09:24 +0200

    feat: Test for saving and loading.

--- tests/test_img.py
+++ tests/test_img.py
@@ -4,7 +4,8 @@ import numpy as np
 from wavelets import wt_2d, wt_2d_inv
 from wavelets.haar import haar_wavelet
 from wavelets.db import db4_wavelet, db8_wavelet, db2_wavelet
-
+from wavelets.io import save_decomposed_img, read_decomposed_img
+import os
 
 @pytest.mark.parametrize("wavelet",
                          [db8_wavelet(), db4_wavelet(), db2_wavelet(),
@@ -48,3 +49,24 @@ def test_2d_rectangular_img(wavelet, level):
 
         assert np.all(np.isclose(inverse, img))
         assert np.all(inverse == img)
+
+
+@pytest.mark.parametrize("path_to_image", [
+     "./resources/maly_rozsutec_2023_grayscale.jpg",
+     "./resources/maly_rozsutec_2023_grayscale_square.jpg"
+])
+@pytest.mark.parametrize("level", [1, 3])
+@pytest.mark.parametrize("wavelet", [db8_wavelet(), haar_wavelet()])
+def test_saving_and_loading(path_to_image, level, wavelet):
+    with Image.open(path_to_image, 'r') as img_pil:
+        image = np.array(img_pil).astype(np.int32)
+
+    decomposition = wt_2d(image, wavelet, level)
+    path = os.path.join('.', 'artifacts', 'saved.tif')
+    save_decomposed_img(decomposition, path)
+    decomposition_found = read_decomposed_img(path)
+    inv_img = wt_2d_inv(decomposition_found, wavelet)
+    assert np.all(np.isclose(inv_img, image))
+    assert np.all(image == inv_img)
+
+
